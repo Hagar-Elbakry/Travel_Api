d9e2386a269077132fdafacee8e0cd557e8d9acb
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-08-20 22:00:52 +0300

    implement public endpoint to get all tours in specific travel

---
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TourResource;
+use App\Models\Travel;
+use Illuminate\Http\Request;
+
+class TourController extends Controller
+{
+    public function index(Travel $travel) {
+        $tours = $travel->tours()->orderBy('starting_date')->paginate(10);
+        return TourResource::collection($tours);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::get('travels', [TravelController::class, 'index']);
+    Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
 });
